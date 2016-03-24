5a02a9f2e2ebad8801bcb206e4632e74ba5c4ed2
Aris Papadopoulos <Aristides.PAPADOPOULOS@sword-group.com>
2016-03-24 22:31:01 +0100

    Prices are shown in list

--- app/src/main/java/com/labsmobile/example/QueriesActivity.java
+++ app/src/main/java/com/labsmobile/example/QueriesActivity.java
@@ -4,16 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.labsmobile.android.model.BalanceQueryResponse;
 import com.labsmobile.android.model.PriceQueryResponse;
 import com.labsmobile.android.service.QueryService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
@@ -32,7 +38,12 @@ public class QueriesActivity extends BaseActivityWithMenu {
     @Bind(R.id.countries)
     EditText countriesBox;
 
+    @Bind(R.id.list_prices)
+    ListView list;
+
+
     private QueryService queryService;
+    private ArrayAdapter<PriceQueryResponse.CountryPriceInfo> adapter;
 
 
     @Override
@@ -42,24 +53,23 @@ public class QueriesActivity extends BaseActivityWithMenu {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ButterKnife.bind(this);
-
-
         queryService = LabsMobileServiceProvider.provideQueryService();
 
+        ButterKnife.bind(this);
 
-        queryBalanceButton.setOnClickListener(new View.OnClickListener() {
+        adapter = new PriceArrayAdapter(this);
+        list.setAdapter(adapter);
+    }
+
+
+    @OnClick(R.id.query_balance_button)
+    public void onBalanceButtonClick() {
+        queryService.queryBalance(new DefaultServiceCallback<BalanceQueryResponse>(QueriesActivity.this) {
             @Override
-            public void onClick(View v) {
-
-                queryService.queryBalance(new DefaultServiceCallback<BalanceQueryResponse>(QueriesActivity.this) {
-                    @Override
-                    public void onResponseOK(BalanceQueryResponse balanceQueryResponse) {
-                        Toast.makeText(QueriesActivity.this,
-                                getResources().getString(R.string.balance_response, balanceQueryResponse.balance),
-                                Toast.LENGTH_LONG).show();
-                    }
-                });
+            public void onResponseOK(BalanceQueryResponse balanceQueryResponse) {
+                Toast.makeText(QueriesActivity.this,
+                        getResources().getString(R.string.balance_response, balanceQueryResponse.balance),
+                        Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -67,18 +77,24 @@ public class QueriesActivity extends BaseActivityWithMenu {
     @OnClick(R.id.query_prices_button)
     public void onPricesButtonClick() {
         progressBar.setVisibility(View.VISIBLE);
+        adapter.clear();
 
         queryService.queryPrices(countriesBox.getText().toString(), new DefaultServiceCallback<PriceQueryResponse>(QueriesActivity.this) {
             @Override
             public void onResponseOK(PriceQueryResponse priceQueryResponse) {
                 progressBar.setVisibility(View.GONE);
+
                 List<PriceQueryResponse.CountryPriceInfo> countryPrices =
                         priceQueryResponse.getCountryPrices();
 
-                Toast.makeText(QueriesActivity.this,
-                        countryPrices.get(0).getName() + " - " + countryPrices.get(0).getPrice(),
-                        Toast.LENGTH_LONG).show();
+                if (countryPrices != null) {
 
+                    adapter.addAll(countryPrices);
+//                    Toast.makeText(QueriesActivity.this,
+//                            countryPrices.get(0).getName() + " - " + countryPrices.get(0).getPrice(),
+//                            Toast.LENGTH_LONG).show();
+
+                }
             }
         });
 
@@ -89,4 +105,31 @@ public class QueriesActivity extends BaseActivityWithMenu {
         return i;
     }
 
+    private class PriceArrayAdapter extends ArrayAdapter<PriceQueryResponse.CountryPriceInfo>{
+        private Context context;
+
+        public PriceArrayAdapter(Context context) {
+            super(context, -1, new ArrayList<PriceQueryResponse.CountryPriceInfo>());
+            this.context = context;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+
+            View rowView = inflater.inflate(R.layout.item_price, parent, false);
+
+            TextView country = (TextView) rowView.findViewById(R.id.list_item_country);
+            TextView prefix = (TextView) rowView.findViewById(R.id.list_item_prefix);
+            TextView price = (TextView) rowView.findViewById(R.id.list_item_price);
+
+
+            country.setText(getItem(position).getName());
+            prefix.setText(getItem(position).getPrefix());
+            price.setText(getItem(position).getPrice() + "");
+
+            return rowView;
+
+        }
+    }
 }
