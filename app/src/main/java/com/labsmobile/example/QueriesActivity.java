f9cae3e75cd8866d9ba74329721e0f37e9400ad5
Aris Papadopoulos <Aristides.PAPADOPOULOS@sword-group.com>
2016-03-24 21:06:44 +0100

    Prices service is called. Results are not shown in the screen yet

--- app/src/main/java/com/labsmobile/example/QueriesActivity.java
+++ app/src/main/java/com/labsmobile/example/QueriesActivity.java
@@ -6,13 +6,19 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.labsmobile.android.model.BalanceQueryResponse;
+import com.labsmobile.android.model.PriceQueryResponse;
 import com.labsmobile.android.service.QueryService;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class QueriesActivity extends BaseActivityWithMenu {
 
@@ -20,6 +26,13 @@ public class QueriesActivity extends BaseActivityWithMenu {
     Button queryBalanceButton;
 
 
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+
+    @Bind(R.id.countries)
+    EditText countriesBox;
+
+    private QueryService queryService;
 
 
     @Override
@@ -32,13 +45,14 @@ public class QueriesActivity extends BaseActivityWithMenu {
         ButterKnife.bind(this);
 
 
-        final QueryService queryService = LabsMobileServiceProvider.provideQueryService();
+        queryService = LabsMobileServiceProvider.provideQueryService();
+
 
         queryBalanceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                queryService.queryBalance(new DefaultServiceCallback<BalanceQueryResponse>(QueriesActivity.this, QueriesActivity.this) {
+                queryService.queryBalance(new DefaultServiceCallback<BalanceQueryResponse>(QueriesActivity.this) {
                     @Override
                     public void onResponseOK(BalanceQueryResponse balanceQueryResponse) {
                         Toast.makeText(QueriesActivity.this,
@@ -48,6 +62,26 @@ public class QueriesActivity extends BaseActivityWithMenu {
                 });
             }
         });
+    }
+
+    @OnClick(R.id.query_prices_button)
+    public void onPricesButtonClick() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        queryService.queryPrices(countriesBox.getText().toString(), new DefaultServiceCallback<PriceQueryResponse>(QueriesActivity.this) {
+            @Override
+            public void onResponseOK(PriceQueryResponse priceQueryResponse) {
+                progressBar.setVisibility(View.GONE);
+                List<PriceQueryResponse.CountryPriceInfo> countryPrices =
+                        priceQueryResponse.getCountryPrices();
+
+                Toast.makeText(QueriesActivity.this,
+                        countryPrices.get(0).getName() + " - " + countryPrices.get(0).getPrice(),
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 
     public static Intent newIntent(Context context) {
